644794076a61dc7273b56d01cda9b4e3067c46bc
Hemp <davidhemphill@users.noreply.github.com>
2023-12-11 23:50:55 -0600

    wip

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::get('/i/{invoice}/download', function (Invoice $invoice) {
 Route::get('/i/{invoice}/dl', function (Invoice $invoice) {
     return response()->streamDownload(function () use ($invoice) {
         echo Browsershot::url(route('invoices.show', $invoice))
-            ->margins(0, 0, 0, 0)
+            ->margins(25, 25, 25, 25)
             ->waitUntilNetworkIdle()
             ->pdf();
     }, "Invoice-#{$invoice->created_at}.pdf", ['Content-Type' => 'application/pdf']);
