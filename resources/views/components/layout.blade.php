37b0744b024fed153da5d221adc5eeb90a2d8706
Hemp <davidhemphill@users.noreply.github.com>
2023-12-12 00:13:37 -0600

    wip

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+    <meta name="viewport" content="width=device-width, user-scalable=no" />
 
     <title>{{ config('app.name') }} - Online Invoice Builder</title>
 
