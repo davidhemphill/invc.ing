990407bf9fa88db9cd27d9a9720abb65dab8c047
Hemp <davidhemphill@users.noreply.github.com>
2023-12-23 12:16:06 -0600

    wip

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,10 @@ import './bootstrap';
 import { Livewire, Alpine } from '../../vendor/livewire/livewire/dist/livewire.esm';
 import mask from '@alpinejs/mask'
 
+Alpine.magic('clipboard', () => subject => {
+    navigator.clipboard.writeText(subject)
+})
+
 // Register any Alpine directives, components, or plugins here...
 Alpine.plugin(mask)
 
