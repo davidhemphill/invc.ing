9d697e1a670fef566fc100706697868a781dc52e
Hemp <davidhemphill@users.noreply.github.com>
2024-02-08 20:59:37 -0600

    wip

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,7 @@
 <!-- Header -->
 <div class="bg-gray-50 dark:bg-gray-900 print:hidden py-4">
     <div class="max-w-[8.5in] mx-auto px-2">
-        <div class="flex flex-col md:justify-between md:flex-row gap-4 screen:md:px-10">
+        <div class="flex flex-col items-baseline md:justify-between md:flex-row gap-4 screen:md:px-10">
             <div class="flex flex-col md:flex-row md:gap-4 items-baseline justify-between">
                 <h1 class="flex flex-col md:flex-row items-baseline gap-1 md:gap-4 text-xl font-bold">
                     <a href="{{ route('home') }}"
@@ -12,7 +12,7 @@
                 <span class="text-xs text-gray-500">free online invoice PDF generator</span>
             </div>
 
-            <p>Want more? Try <a href="https://pushsilver.com">Pushsilver</a></p>
+            <p class="text-sm text-right text-gray-600 dark:text-white">Want more? Try <a class="text-purple-500 hover:underline" href="https://pushsilver.com">Pushsilver</a></p>
         </div>
     </div>
 </div>
